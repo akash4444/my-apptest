028af1bc0f52a7238ccb945e6efa42cc31f2a34a
akash4444 <akashkumarkale@gmail.com>
2024-03-25 19:21:30 +0530

    config chnages

--- src/config.js
+++ src/config.js
@@ -1,3 +1,9 @@
-const servicePath = process.env.SERVICE_PATH || "http://localhost:3000";
+let servicePath = "";
 
+const isProduction = process.env.NODE_ENV === "production";
+if (isProduction) {
+  servicePath = "";
+} else {
+  servicePath = "http://localhost:3000";
+}
 export default servicePath;
